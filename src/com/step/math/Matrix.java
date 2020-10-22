1efb3a9f2977cceaec61220939a1d8bfd147db2b
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-10-22 15:48:20 +0530

    added message to be displayed on failure

--- src/com/step/math/Matrix.java
+++ src/com/step/math/Matrix.java
@@ -5,30 +5,30 @@ public class Matrix {
   private int col;
   private int[][] data;
 
-  public Matrix(int row,int col){
+  public Matrix(int row, int col) {
     this.row = row;
     this.col = col;
     this.data = new int[row][col];
   }
 
-  public Matrix(int row,int col,int[][] data){
+  public Matrix(int row, int col, int[][] data) {
     this.row = row;
     this.col = col;
     this.data = new int[row][col];
-    for (int i = 0; i < row; i++){
-      for (int j = 0; j < col; j++){
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
         this.data[i][j] = data[i][j];
       }
     }
   }
 
   public Matrix add(Matrix matrix) {
-    if (matrix.row != this.row || matrix.col != this.col){
+    if (matrix.row != this.row || matrix.col != this.col) {
       return null;
     }
-    Matrix result = new Matrix(this.row,this.col);
-    for (int i = 0; i < this.row; i++){
-      for (int j = 0; j < this.col; j++){
+    Matrix result = new Matrix(this.row, this.col);
+    for (int i = 0; i < this.row; i++) {
+      for (int j = 0; j < this.col; j++) {
         result.data[i][j] = matrix.data[i][j] + this.data[i][j];
       }
     }
@@ -36,27 +36,26 @@ public class Matrix {
   }
 
   public Matrix subtract(Matrix matrix) {
-    if (matrix.row != this.row || matrix.col != this.col){
+    if (matrix.row != this.row || matrix.col != this.col) {
       return null;
     }
-    Matrix result = new Matrix(this.row,this.col);
-    for (int i = 0; i < this.row; i++){
-      for (int j = 0; j < this.col; j++){
+    Matrix result = new Matrix(this.row, this.col);
+    for (int i = 0; i < this.row; i++) {
+      for (int j = 0; j < this.col; j++) {
         result.data[i][j] = this.data[i][j] - matrix.data[i][j];
       }
     }
     return result;
   }
 
-
   public Matrix multiply(Matrix matrix) {
-    if(this.col != matrix.row){
+    if (this.col != matrix.row) {
       return null;
     }
-    Matrix result = new Matrix(this.row,matrix.col);
-    for (int i = 0; i < this.row; i++){
-      for (int j = 0; j < matrix.col; j++){
-        for (int k = 0; k < matrix.row; k++){
+    Matrix result = new Matrix(this.row, matrix.col);
+    for (int i = 0; i < this.row; i++) {
+      for (int j = 0; j < matrix.col; j++) {
+        for (int k = 0; k < matrix.row; k++) {
           result.data[i][j] += (this.data[i][k] * matrix.data[k][j]);
         }
       }
@@ -64,17 +63,16 @@ public class Matrix {
     return result;
   }
 
-  private boolean isDeepEqual(Matrix matrix){
-    for (int i = 0; i <this.row; i++){
-      for (int j = 0; j <this.col; j++){
-        if(this.data[i][j] != matrix.data[i][j]){
+  private boolean isDeepEqual(Matrix matrix) {
+    for (int i = 0; i < this.row; i++) {
+      for (int j = 0; j < this.col; j++) {
+        if (this.data[i][j] != matrix.data[i][j]) {
           return false;
         }
       }
     }
     return true;
   }
-
 
   @Override
   public String toString() {
@@ -98,12 +96,10 @@ public class Matrix {
     if (!(other instanceof Matrix)) {
       return false;
     }
-    Matrix matrix2  = (Matrix)other;
+    Matrix matrix2 = (Matrix) other;
     if (this.row != matrix2.row || this.col != matrix2.col) {
       return false;
     }
     return this.isDeepEqual(matrix2);
   }
 }
-
-
--- test/com/step/math/MatrixTest.java
+++ test/com/step/math/MatrixTest.java
@@ -1,130 +1,150 @@
 package com.step.math;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-
 public class MatrixTest {
+
   @Test
   public void representsMatrixInString() {
-    int[][] elements = {{1,2},{3,4}};
-    Matrix matrix = new Matrix(2,2,elements);
+    int[][] elements = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix = new Matrix(2, 2, elements);
     String actual = matrix.toString();
     String expected = "Matrix {\n  1 2 \n  3 4 \n}";
-    assertEquals(actual, expected);
+    String message = "should give textual representation of given Matrix";
+    assertEquals(message, actual, expected);
   }
 
   @Test
   public void equatesTwoMatricesThatAreEqual() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{1,2},{3,4}};
-    Matrix matrix2 = new Matrix(2,2,elements2);
-    assertTrue(matrix1.equals(matrix2));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix2 = new Matrix(2, 2, elements2);
+    String message = "should give true on equating two equal matrices ";
+    assertTrue(message, matrix1.equals(matrix2));
   }
 
   @Test
   public void equatesTwoMatricesThatAreSameInstance() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    assertTrue(matrix1.equals(matrix1));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    String message = "should give true on equating same matrix instance";
+    assertTrue(message, matrix1.equals(matrix1));
   }
 
   @Test
   public void equatesTwoMatricesThatAreNotEqual() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{3,2},{3,4}};
-    Matrix matrix2 = new Matrix(2,2,elements2);
-    assertFalse( matrix1.equals(matrix2));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 3, 2 }, { 3, 4 } };
+    Matrix matrix2 = new Matrix(2, 2, elements2);
+    String message =
+      "should give false on equating matrices with different elements";
+    assertFalse(message, matrix1.equals(matrix2));
   }
 
   @Test
   public void equatesTwoMatricesThatAreNotEqualInDimensions() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{3,2},{3,4},{4,5}};
-    Matrix matrix2 = new Matrix(3,2,elements2);
-    assertFalse(matrix1.equals(matrix2));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 3, 2 }, { 3, 4 }, { 4, 5 } };
+    Matrix matrix2 = new Matrix(3, 2, elements2);
+    String message =
+      "should give false on equating matrices with different dimensions";
+    assertFalse(message, matrix1.equals(matrix2));
   }
 
   @Test
   public void equatesMatricesWithOther() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{3,2},{3,4},{4,5}};
-    assertFalse(matrix1.equals(elements2));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 1, 2 }, { 3, 4 } };
+    String message = "should give false on equating matrix with other instance";
+    assertFalse(message, matrix1.equals(elements2));
   }
 
   @Test
   public void addsTwoMatrices() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{2,1},{4,3}};
-    Matrix matrix2 = new Matrix(2,2,elements2);
-    int[][] elements3 = {{3,3},{7,7}};
-    Matrix resultantMatrix = new Matrix(2,2,elements3);
-    assertEquals(resultantMatrix,matrix1.add(matrix2));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 2, 1 }, { 4, 3 } };
+    Matrix matrix2 = new Matrix(2, 2, elements2);
+    int[][] elements3 = { { 3, 3 }, { 7, 7 } };
+    Matrix resultantMatrix = new Matrix(2, 2, elements3);
+    String message = "should give sum of two matrices of same dimensions";
+    assertEquals(message, resultantMatrix, matrix1.add(matrix2));
   }
 
   @Test
   public void doesNotAddMatricesOfDifferentDimensions() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{2,1},{4,3},{5,6}};
-    Matrix matrix2 = new Matrix(3,2,elements2);
-    assertNull(matrix1.add(matrix2));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 2, 1 }, { 4, 3 }, { 5, 6 } };
+    Matrix matrix2 = new Matrix(3, 2, elements2);
+    String message =
+      "should give null on adding matrices of different dimensions";
+    assertNull(message, matrix1.add(matrix2));
   }
 
   @Test
   public void subtractsTwoMatrices() {
-    int[][] elements1 = {{2,5},{4,3}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{1,2},{3,4}};
-    Matrix matrix2 = new Matrix(2,2,elements2);
-    int[][] elements3 = {{1,3},{1,-1}};
-    Matrix resultantMatrix = new Matrix(2,2,elements3);
-    assertEquals(resultantMatrix,matrix1.subtract(matrix2));
+    int[][] elements1 = { { 2, 5 }, { 4, 3 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix2 = new Matrix(2, 2, elements2);
+    int[][] elements3 = { { 1, 3 }, { 1, -1 } };
+    Matrix resultantMatrix = new Matrix(2, 2, elements3);
+    String message =
+      "should give difference of two matrices of same dimensions";
+    assertEquals(message, resultantMatrix, matrix1.subtract(matrix2));
   }
 
   @Test
   public void doesNotSubtractMatricesOfDifferentDimensions() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{2,1},{4,3},{5,6}};
-    Matrix matrix2 = new Matrix(3,2,elements2);
-    assertNull(matrix1.subtract(matrix2));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 2, 1 }, { 4, 3 }, { 5, 6 } };
+    Matrix matrix2 = new Matrix(3, 2, elements2);
+    String message =
+      "should give null on subtracting matrices of different dimensions";
+    assertNull(message, matrix1.subtract(matrix2));
   }
 
   @Test
   public void multipliesTwoMatrices() {
-    int[][] elements1 = {{2,5},{4,3}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{1,2},{3,4}};
-    Matrix matrix2 = new Matrix(2,2,elements2);
-    int[][] elements3 = {{17,24},{13,20}};
-    Matrix resultantMatrix = new Matrix(2,2,elements3);
-    assertEquals(resultantMatrix,matrix1.multiply(matrix2));
+    int[][] elements1 = { { 2, 5 }, { 4, 3 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix2 = new Matrix(2, 2, elements2);
+    int[][] elements3 = { { 17, 24 }, { 13, 20 } };
+    Matrix resultantMatrix = new Matrix(2, 2, elements3);
+    String message = "should give product of two matrices of same dimensions";
+    assertEquals(message, resultantMatrix, matrix1.multiply(matrix2));
   }
 
   @Test
   public void multipliesTwoMatricesWithDifferentDimension() {
-    int[][] elements1 = {{1,2,3},{4,5,6}};
-    Matrix matrix1 = new Matrix(2,3,elements1);
-    int[][] elements2 = {{7,8},{9,10},{11,12}};
-    Matrix matrix2 = new Matrix(3,2,elements2);
-    int[][] elements3 = {{58,64},{139,154}};
-    Matrix resultantMatrix = new Matrix(2,2,elements3);
-    assertEquals(resultantMatrix,matrix1.multiply(matrix2));
+    int[][] elements1 = { { 1, 2, 3 }, { 4, 5, 6 } };
+    Matrix matrix1 = new Matrix(2, 3, elements1);
+    int[][] elements2 = { { 7, 8 }, { 9, 10 }, { 11, 12 } };
+    Matrix matrix2 = new Matrix(3, 2, elements2);
+    int[][] elements3 = { { 58, 64 }, { 139, 154 } };
+    Matrix resultantMatrix = new Matrix(2, 2, elements3);
+    String message =
+      "should give product of two matrices if column of matrixA is equal to row of matrixB";
+    assertEquals(message, resultantMatrix, matrix1.multiply(matrix2));
   }
-
 
   @Test
   public void doesNotMultiplyMatricesWithWrongDimension() {
-    int[][] elements1 = {{1,2},{3,4}};
-    Matrix matrix1 = new Matrix(2,2,elements1);
-    int[][] elements2 = {{2,1},{4,3},{5,6}};
-    Matrix matrix2 = new Matrix(3,2,elements2);
-    assertNull(matrix1.multiply(matrix2));
+    int[][] elements1 = { { 1, 2 }, { 3, 4 } };
+    Matrix matrix1 = new Matrix(2, 2, elements1);
+    int[][] elements2 = { { 2, 1 }, { 4, 3 }, { 5, 6 } };
+    Matrix matrix2 = new Matrix(3, 2, elements2);
+    String message =
+      "should give null of two matrices if column of matrixA is not equal to row of matrixB";
+    assertNull(message, matrix1.multiply(matrix2));
   }
 }
