97f53e691c6d019f2e514cb8e828950bc8e89625
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-10-24 12:34:22 +0530

    removed unnecessary constructor

--- src/com/step/math/Matrix.java
+++ src/com/step/math/Matrix.java
@@ -5,12 +5,6 @@ public class Matrix {
   private int col;
   private int[][] data;
 
-  public Matrix(int row, int col) {
-    this.row = row;
-    this.col = col;
-    this.data = new int[row][col];
-  }
-
   public Matrix(int row, int col, int[][] data) {
     this.row = row;
     this.col = col;
@@ -26,41 +20,41 @@ public class Matrix {
     if (matrix.row != this.row || matrix.col != this.col) {
       return null;
     }
-    Matrix result = new Matrix(this.row, this.col);
+    int[][] result = new int[this.row][this.col];
     for (int i = 0; i < this.row; i++) {
       for (int j = 0; j < this.col; j++) {
-        result.data[i][j] = matrix.data[i][j] + this.data[i][j];
+        result[i][j] = matrix.data[i][j] + this.data[i][j];
       }
     }
-    return result;
+    return new Matrix(this.row, this.col, result);
   }
 
   public Matrix subtract(Matrix matrix) {
     if (matrix.row != this.row || matrix.col != this.col) {
       return null;
     }
-    Matrix result = new Matrix(this.row, this.col);
+    int[][] result = new int[this.row][this.col];
     for (int i = 0; i < this.row; i++) {
       for (int j = 0; j < this.col; j++) {
-        result.data[i][j] = this.data[i][j] - matrix.data[i][j];
+        result[i][j] = this.data[i][j] - matrix.data[i][j];
       }
     }
-    return result;
+    return new Matrix(this.row, this.col, result);
   }
 
   public Matrix multiply(Matrix matrix) {
     if (this.col != matrix.row) {
       return null;
     }
-    Matrix result = new Matrix(this.row, matrix.col);
+    int[][] result = new int[this.row][this.col];
     for (int i = 0; i < this.row; i++) {
       for (int j = 0; j < matrix.col; j++) {
         for (int k = 0; k < matrix.row; k++) {
-          result.data[i][j] += (this.data[i][k] * matrix.data[k][j]);
+          result[i][j] += (this.data[i][k] * matrix.data[k][j]);
         }
       }
     }
-    return result;
+    return new Matrix(this.row, this.col, result);
   }
 
   private boolean isDeepEqual(Matrix matrix) {
