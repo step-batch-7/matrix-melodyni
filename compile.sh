4cca85f596c031de7cbfe412acc6930cff1ce552
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-10-24 12:23:28 +0530

    updated scripts for compiling and running tests
    It can now run tests for multiple classes
    added set -euo for safety on failure

--- compile.sh
+++ compile.sh
@@ -1,2 +1,8 @@
-rm -rf out/*;
-javac -d out -cp .:out:lib/junit-4.13.1.jar:lib/hamcrest-core-1.3.jar $(find . -name "*.java");
+# /bin/sh
+set -e;
+
+echo "cleaning...."
+rm -rf out;
+mkdir out;
+echo "compiling....\n" 
+javac -d out -cp out:lib/junit-4.13.1.jar:lib/hamcrest-core-1.3.jar $(find . -name "*.java");
--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-java -cp out:lib/junit-4.13.1.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore com.step.math.MatrixTest;
+# /bin/sh
+set -euo pipefail;
+
+TESTNAMES=`find test -name "*Test.java" | sed "s/test\///" | sed "s/.java//" | tr "/" "."`
+
+echo "running tests....\n";
+java -cp out:lib/junit-4.13.1.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore $TESTNAMES;
